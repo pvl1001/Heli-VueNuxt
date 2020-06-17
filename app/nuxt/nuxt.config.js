adfd6cf4449bfa9be77b53700de99b2a1ebbaa4b
Igor Vetrov <ceo@igorvetrov.ru>
2020-06-17 14:22:01 +0300

    add nuxt

--- app/nuxt/nuxt.config.js
+++ app/nuxt/nuxt.config.js
@@ -36,14 +36,12 @@ module.exports = {
     ** Run ESLint on save
     */
     extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
+            enforce: 'pre',
+            test: /\.(js|vue)$/,
+            loader: 'eslint-loader',
+            exclude: /(node_modules)/
         })
-      }
     }
   }
 }
