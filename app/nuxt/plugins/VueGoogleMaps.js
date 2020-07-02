f17beaaecb22da9557cb34ac53bdf58cc1fb8120
pvl1001 <67624599+pvl1001@users.noreply.github.com>
2020-07-02 11:04:50 +0300

    Update VueGoogleMaps.js

--- app/nuxt/plugins/VueGoogleMaps.js
+++ app/nuxt/plugins/VueGoogleMaps.js
@@ -3,7 +3,7 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
     load: {
-        key: 'AIzaSyDKcw_AVzYLqsuW5lL3-PJRBreB1MwKp-s',
+        key: '',
         libraries: 'places', // This is required if you use the Autocomplete plugin
         // OR: libraries: 'places,drawing'
         // OR: libraries: 'places,drawing,visualization'
